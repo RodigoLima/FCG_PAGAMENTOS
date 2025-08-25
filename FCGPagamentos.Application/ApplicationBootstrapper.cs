59f3aed98b8d697d40ad9f215376b58a7da0fe6c
Renato FIAP <RM360915@fiap.com.br>
2025-08-25 19:13:38 -0300

    refactor all
    Creating a new project with all we need and it's not gonna generate all those files of Copilot or anything else

---
+++ FCGPagamentos.Application/ApplicationBootstrapper.cs
@@ -0,0 +1,13 @@
+﻿using FCGPagamentos.Application.Repository;
+using FCGPagamentos.Application.UseCases;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FCGPagamentos.Application;
+
+public static class ApplicationBootstrapper
+{
+  public static void Register(this IServiceCollection services)
+  {
+    services.AddTransient<IAuthenticationUseCase, AuthenticationUseCase>();
+  }
+}
--- FCGPagamentos/FCGPagamentos.Infrastructure/Repository/AuthenticationRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using FCGPagamentos.Application.Repository;
-
-namespace FCGPagamentos.Infrastructure.Repository;
-internal class AuthenticationRepository : IAuthenticationRepository
-{
-  // Esta camada iria acessar banco de dados e tals
-  bool IAuthenticationRepository.Authenticate(string username, string password) => true;
-}
