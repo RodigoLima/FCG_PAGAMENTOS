bfdee5e22f9a05279b2c809ea802e3d2dade1f25
Rodrigo <ferreirarodrigo230@gmail.com>
2025-08-29 17:10:44 -0400

    Add Swagger support and update logging level
    Updated `Program.cs` to include Swagger services and
    configure them for the Development environment. Added
    custom payment endpoint mapping. Modified
    `appsettings.Development.json` to change the logging
    level for `Microsoft.AspNetCore` from "Warning" to
    "Information" for improved logging during development.

--- src/FCGPagamentos.API/Program.cs
+++ src/FCGPagamentos.API/Program.cs
@@ -2,8 +2,24 @@ using FCGPagamentos.API.DI;
 using FCGPagamentos.API.Endpoints;
 
 var b = WebApplication.CreateBuilder(args);
+
+// Serviços da aplicação
 b.Services.AddAppServices(b.Configuration);
 
+// Adiciona Swagger
+b.Services.AddEndpointsApiExplorer();
+b.Services.AddSwaggerGen();
+
 var app = b.Build();
+
+// Ativa Swagger só em Development
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// Seus endpoints customizados
 app.MapPaymentEndpoints();
+
 app.Run();
